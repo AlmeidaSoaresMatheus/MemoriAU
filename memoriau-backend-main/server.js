812f6395a10534ba8ee4802f3e29c762bb82d34b
Carolineleless <carolineleless@gmail.com>
2024-05-19 19:51:18 -0400

    adicionando configuracao do S3

--- memoriau-backend-main/server.js
+++ memoriau-backend-main/server.js
@@ -2,6 +2,7 @@ require('dotenv').config({path:'variaveis.env'});
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
+require('dotenv').config();
 
 const routes = require('./routes');
 
