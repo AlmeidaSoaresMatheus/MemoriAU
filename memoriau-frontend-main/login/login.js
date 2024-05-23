4e3920ae3e30c94278f40ab1c0845bb3893420b5
Elizeu Reis <87556430+elizeureisl@users.noreply.github.com>
2024-05-22 22:04:22 -0300

    incluindo funcão de login

--- memoriau-frontend-main/login/login.js
+++ memoriau-frontend-main/login/login.js
@@ -1,27 +1,26 @@
-document.getElementById('signup-form').addEventListener('submit', async function(event) {
+document.getElementById('user-login-form').addEventListener('submit', async function(event) {
     event.preventDefault(); // Previne o envio padrão do formulário
 
     const formData = new FormData(this);
-    const name = formData.get('name');
     const email = formData.get('email');
     const password = formData.get('password');
 
     try {
         const requestBody = new URLSearchParams();
-        requestBody.append('name', name);
         requestBody.append('email', email);
         requestBody.append('password', password);
 
-        const response = await fetch('http://localhost:3306/api/users', {
+        const response = await fetch('http://localhost:3306/api/users/verifyLogin', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/x-www-form-urlencoded' // Adjusted Content-Type
             }, 
-            body: requestBody // Adjusted request body
+            body: requestBody 
         });
 
         if (!response.ok) {
-            throw new Error('Failed to register user');
+            throw new Error('Failed to find user');
+            // flagLoginError()
         }
         else {
             const newUser = await response.json();
@@ -31,24 +30,35 @@ document.getElementById('signup-form').addEventListener('submit', async function
         }
     } catch (error) {
         console.error('Error:', error);
-        alert('Failed to register user');
+        flagLoginError()
+        // alert('Failed to find user');
     }
 });
 
-async function checkUserExists(email) {
-    try {
-        const response = await fetch(`http://localhost:3306/api/users?email=${email}`);
-        if (!response.ok) {
-            throw new Error('Erro ao verificar usuário');
-        }
-        const data = await response.json();
-        return data.exists;
-    } catch (error) {
-        console.error('Erro:', error);
-
-        return false;
-    }
+function flagLoginError() {
+    const flag = document.getElementById('flag');
+    const parentDiv = flag.parentNode;
+    const listItem = document.createElement('p');
+    listItem.textContent = `email e/ou senha não existe.`;
+    parentDiv.replaceChild(listItem, flag);
 }
+
+
+
+// async function checkUserExists(email) {
+//     try {
+//         const response = await fetch(`http://localhost:3306/api/users?email=${email}`);
+//         if (!response.ok) {
+//             throw new Error('Erro ao verificar usuário');
+//         }
+//         const data = await response.json();
+//         return data.exists;
+//     } catch (error) {
+//         console.error('Erro:', error);
+
+//         return false;
+//     }
+// }
 
 
 
