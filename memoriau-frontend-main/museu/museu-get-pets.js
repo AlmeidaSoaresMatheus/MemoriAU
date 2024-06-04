bd41addae506afbc4b6325313627d5ba7b13569d
Elizeu Reis <87556430+elizeureisl@users.noreply.github.com>
2024-06-04 08:58:41 -0300

    melhorias

---
+++ memoriau-frontend-main/museu/museu-get-pets.js
@@ -0,0 +1,36 @@
+document.addEventListener('DOMContentLoaded', async function() {
+
+    try {
+        const email = localStorage.getItem('Login');
+        var carousel = document.getElementById("imagePetName");
+        carousel.innerHTML = '';
+
+        const response = await fetch(`http://localhost:3306/api/pets/find?email=${email}`, {
+            method: 'GET',
+        });
+
+        const data = await response.json();
+
+        const petNames = data.pets.map(pet => pet.name);
+        console.log(petNames)
+
+        if (!response.ok) {
+            throw new Error('Failed to find user animals');
+        }
+
+        for (let i = 0; i < petNames.length; i++) {
+            const petName = petNames[i]
+
+            const option = document.createElement('option');
+
+            option.value = petName
+            option.innerText = petName
+
+            carousel.appendChild(option);
+        }
+    } catch (error) {
+        console.error('Error:', error);
+    } 
+
+});
+
